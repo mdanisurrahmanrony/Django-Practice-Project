20e2e38e76d8e95e6ac009c27bde4410f55c8291
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-10 17:27:08 +0600

    Models Overview and Creating Models

--- django_practice_app/models.py
+++ django_practice_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Musician(models.Model):
+    # id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    # id = models.AutoField(primary_key=True)
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()

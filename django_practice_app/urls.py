14aaacca62a1d85173f176aaf5370047ba867376
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-10 16:42:35 +0600

    Templates

--- django_practice_app/urls.py
+++ django_practice_app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from django_practice_app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.contact, name='contact'),
+    path('', views.index, name='index'),    
 ]
--- django_practice_app/views.py
+++ django_practice_app/views.py
@@ -4,7 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>I am from django_practice_app</h1>")
-
-def contact(request):
-    return HttpResponse("<h1>I am from contact page</h1>")
+    diction = {'text_1':'I am learning django'}
+    return render(request, 'django_practice_app/index.html', context=diction)

d12699229c20b5b5fd28faf72367fd386338b368
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-10 18:41:15 +0600

    Views, Templates and Models

--- django_practice_app/views.py
+++ django_practice_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_practice_app.models import Musician, Album
 
 # Create your views here.
 
 def index(request):
-    diction = {'text_1':'I am learning django'}
+    # SELECT * FROM Musician ORDER BY first_name
+    musician_list = Musician.objects.order_by('first_name')
+    diction = {'text_1':'This is a list of Musician', 'musician': musician_list}
     return render(request, 'django_practice_app/index.html', context=diction)

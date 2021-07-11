79a15cac4eb17ef2dad5e209adb9a7e7c98d82e5
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-11 12:06:26 +0600

    Creating Django Form 3

--- django_practice_app/forms.py
+++ django_practice_app/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
 class user_form(forms.Form):
-    user_name = forms.CharField(required=True, label="Full Name", initial="Rony")
+    user_name = forms.CharField(label="Full Name", widget = forms.TextInput(attrs={'placeholder':'Enter your full name', 'style':'width:300px'}))
+
+    user_dob = forms.DateField(label="Date of Birth", widget = forms.TextInput(attrs={'type':'date'}))
+
     user_email = forms.EmailField(label="User Email")

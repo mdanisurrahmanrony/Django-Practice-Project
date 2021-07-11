bfb2bbd77d778e7c3a27b5099260b8fc0ee00c75
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-11 11:37:26 +0600

    Creating a Django Form 2

--- django_practice_app/forms.py
+++ django_practice_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class user_form(forms.Form):
-    user_name = forms.CharField()
-    user_email = forms.EmailField()
+    user_name = forms.CharField(required=True, label="Full Name", initial="Rony")
+    user_email = forms.EmailField(label="User Email")

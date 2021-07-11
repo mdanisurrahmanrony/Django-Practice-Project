bfab2521253c5ee0b275340064d69cc38c2c921c
Md. Anisur Rahman Rony <52064058+mdanisurrahmanrony@users.noreply.github.com>
2021-07-11 11:20:44 +0600

    Creatng a Django Form 1

--- django_practice_app/views.py
+++ django_practice_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django_practice_app.models import Musician, Album
+from django_practice_app import forms
 
 # Create your views here.
 
@@ -11,5 +12,6 @@ def index(request):
     return render(request, 'django_practice_app/index.html', context=diction)
 
 def form(request):
-    diction = {}
+    new_form = forms.user_form()
+    diction = {'test_form' : new_form, 'heading_1' : "This form is created using django library"}
     return render(request, 'django_practice_app/form.html', context=diction)
